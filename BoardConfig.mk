da426f9ee18fea8ebcc897c32e826353b00828dd
crancocco <shulaibai@hustunique.com>
2016-12-11 17:00:10 +0800

    capricorn: update dt2w node

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,7 +118,7 @@ BOARD_HARDWARE_CLASS += \
     hardware/cyanogen/cmhw \
     $(DEVICE_PATH)/cmhw
 BOARD_USES_CYANOGEN_HARDWARE := true
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc/75ba000.i2c/i2c-12/12-0020/input/input2/wake_gesture"
 
 # CNE and DPM
 TARGET_LDPRELOAD := libNimsWrap.so
